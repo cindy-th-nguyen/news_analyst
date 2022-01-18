d4800dc120987b9547c08457f62dc5c501d4a8dc
MoussaOudjama <moussa.oudjama@canal-plus.com>
2022-01-18 16:46:02 +0100

    [WIP] review prof + ajout max_features

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import matplotlib.pyplot as plt #on laisse dans le doute
+import matplotlib.pyplot as plt # On laisse dans le doute
 from sklearn.neural_network import MLPClassifier
 import pandas as pand
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
 
-    #lecture du csv avec le framework pandas
+    # Lecture du csv avec le framework pandas
     data = pand.read_csv('news.csv')
     print(f'data shape : {data.shape}')
     inputs = data['text']
@@ -17,34 +16,37 @@ if __name__ == "__main__":
     outputs = data['label']
     print(f'label shape : {outputs.shape}')
 
-    isFakeLabels = []
+    isFakeLabel = []
     for i in range(len(outputs)):
         if outputs[i] == 'FAKE':
-            isFakeLabels.append(0)
+            isFakeLabel.append(1)
         else:
-            isFakeLabels.append(1)
+            isFakeLabel.append(0)
 
-    #On split le JDD
-    train_article_inputs, test_articles, train_article_desired, test_article_desired = train_test_split(data['text'], isFakeLabels, test_size=0.1, random_state=0)
+    # On split le JDD
+    train_article_inputs, test_articles, train_article_desired, test_article_desired = train_test_split(data['text'], isFakeLabel, test_size=0.1, random_state=0)
 
-    #Methode TFIDF pour lire la réccurence des mots, fréquence.
-    #Vectorisation du text
-    tfidf = TfidfVectorizer()
+    # Methode TFIDF pour lire la réccurence des mots, fréquence. 
+    # Vectorisation de la pensée
+    # Voir les différents paramètres possible dans la DOC
+    tfidf = TfidfVectorizer(max_features=21405)
 
     tfidf_train = tfidf.fit_transform(train_article_inputs) 
     tfidf_test = tfidf.transform(test_articles)
-    tfidf_test_final = tfidf.transform(data['text'])
 
     # Classification -> Faut trouver un algo de classification, VOIR AVEC LE PROF
-    # Trouver un Classifier qui convient
     mlpClassifer = MLPClassifier()
     mlpClassifer.fit(tfidf_train, train_article_desired)
+    print(f'coef len: {len(mlpClassifer.coefs_[0])}')
     prediction = mlpClassifer.predict(tfidf_test)
 
-    #Score
+    # Score
     score = accuracy_score(test_article_desired, prediction)
-    print(f'score: {round(score * 100, 2)}%')
+    # s = mlpClassifer.score(test_article_desired,prediction)
+    print(f'score: {score}')
 
-    #Matrice de confusion
+    # Matrice de confusion
     cmat = confusion_matrix(test_article_desired,prediction)
     print(cmat)
+
+    
